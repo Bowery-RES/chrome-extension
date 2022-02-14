f92f0f0e11359993348fc5f9ca5e21fcd3032255
mrromro <insigniasane@gmail.com>
2022-02-14 19:57:42 +0300

    Revert "Error output"
    This reverts commit b69edd1c8c3a7276acfd8c92a6a98f4fcc234fa8.

--- scripts/env.js
+++ scripts/env.js
@@ -1,8 +1,4 @@
-const result = require('dotenv').config()
-
-if (result.error) {
-  throw result.error
-}
+require('dotenv').config()
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
